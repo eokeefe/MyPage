c4da9d71edb2eedb64a1f2ace3077fae8dc5074a
Erik O'Keefe <erik.t.okeefe@gmail.com>
2014-11-22 20:37:03 -0800

    fullscreen
    partial setup of fullscreen package.

---
+++ packages/eokeefe:fullscreen/package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'eokeefe:fullscreen',
+  summary: 'Responsive parallax background images with content centering and overflow',
+  version: '0.1.0'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use('jquery', 'client')
+  api.addFiles('eokeefe:fullscreen.js', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('eokeefe:fullscreen');
+  api.addFiles('eokeefe:fullscreen-tests.js', 'client');
+});
