a36189eaf52f4cf63406958b41b22f6d24a0adf1
Erik O'Keefe <erik.t.okeefe@gmail.com>
2014-12-01 00:27:50 -0800

    Method removed

--- packages/eokeefe:fullscreen/lib/src/eokeefe:fullscreen.js
+++ packages/eokeefe:fullscreen/lib/src/eokeefe:fullscreen.js
@@ -1,6 +1,26 @@
 fullscreen = function() {
   var fullscreen = {version: "0.3"};
 
+  fullscreen.setup = function () {
+    /* detect touch */
+    if("ontouchstart" in window) {
+      document.documentElement.className = document.documentElement.className + " touch";
+    }
+
+    $(window).resize(fullscreen.resize);
+    $(window).focus(fullscreen.resize);
+
+    if(!$("html").hasClass("touch")){
+      $(".parallax").css("background-attachment", "fixed");
+      $(window).resize(fullscreen.parallax);
+      //$(window).focus(fullscreen.parallax);
+      $(window).scroll(fullscreen.parallax);
+      fullscreen.parallax();
+    }
+
+    fullscreen.resize();
+  };
+
   /* resize background images */
   // backgroundResize = function (){
   fullscreen.resize = function () {
